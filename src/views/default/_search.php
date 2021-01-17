1b929491f41b0e6082417736557401031bfd28cc
Arda Talu <arda.starkiler@gmail.com>
2021-01-17 23:12:39 +0300

    store module updated

--- src/views/default/_search.php
+++ src/views/default/_search.php
@@ -23,7 +23,7 @@ use yii\widgets\ActiveForm;
 
     <?= $form->field($model, 'price') ?>
 
-    <?= $form->field($model, 'created_at') ?>
+    <?php // echo $form->field($model, 'created_at') ?>
 
     <?php // echo $form->field($model, 'updated_at') ?>
 
