b7720a1f11d115179c6048ad1d72e6ce23b72a89
Arda Talu <arda.starkiler@gmail.com>
2021-01-17 05:53:32 +0300

    project updated

--- src/Module.php
+++ src/Module.php
@@ -10,7 +10,7 @@ class Module extends \yii\base\Module
     /**
      * {@inheritdoc}
      */
-    public $controllerNamespace = 'deadpoolzephyr\computer\controllers';
+    public $controllerNamespace = 'deadpoolzephyr\store\controllers';
 
     /**
      * {@inheritdoc}
